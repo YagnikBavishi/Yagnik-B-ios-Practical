47ddab40282333371a46de74507c5987c2269ef2
YagnikBavishi <yagnikbavishi003@gmail.com>
2022-04-27 16:58:10 +0530

    Completed TE5-T439_Basic_Auto_Layout_Modification

--- ViewControllers/LoginPageViewController.swift
+++ ViewControllers/LoginPageViewController.swift
@@ -4,14 +4,47 @@ class LoginPageViewController: UIViewController {
     
     //MARK: - Outlets
     @IBOutlet weak var lblLogin: UILabel!
- 
+    @IBOutlet weak var tfFirstName: UITextField!
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    // MARK: - Variables
+    var response: String?
+    
     //MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTapAround()
         addGuesture()
     }
     
-    //MARK: - Actions
+    // MARK: - File Private Functions
+    fileprivate func validateFields() -> String? {
+        if let firstName = tfFirstName.text, let email = tfEmail.text, let password = tfPassword.text {
+            if(firstName.isEmpty) {
+                return "First Name is Empty"
+            } else if(email.isEmpty) {
+                return "Email is Empty"
+            } else if (!(email.contains(".")) || !(email.contains("@"))) {
+                return "Wrong email"
+            } else if(password.isEmpty) {
+                return "Password is Empty"
+            }
+            return nil
+        }
+        return nil
+    }
+    
+    // MARK: - Actions
+    @IBAction func btnSignup(_ sender: UIButton) {
+        response = validateFields()
+        guard let response = response else {
+            Alerts.customAlert(message: "Success", body:"" , viewController: self)
+            return
+        }
+        Alerts.customAlert(message: response, body:"" , viewController: self)
+    }
+
     @IBAction func goBackToHome(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
@@ -33,5 +66,22 @@ extension LoginPageViewController {
         }
     }
     
+}// End of Extension
+
+// MARK: - UITextFieldDelegate
+extension LoginPageViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case self.tfFirstName:
+            self.tfEmail.becomeFirstResponder()
+        case self.tfEmail:
+            self.tfPassword.becomeFirstResponder()
+        default:
+            self.tfPassword.resignFirstResponder()
+        }
+        return true
+    }
+
 }// End of Extension
 
